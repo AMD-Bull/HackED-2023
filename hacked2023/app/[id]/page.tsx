4239dc3f27deacf172f04b7339acdc14272c29ed
Riley Zilka <rzilka@rodeoready.com>
2023-01-07 15:45:50 -0700

    progress

---
+++ hacked2023/app/[id]/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css"
+import React from 'react'
+
+export default function Class({param}) {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
--- hacked2023/app/layout.tsx
+++ hacked2023/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import NavBar from './navbar'
 
 export default function RootLayout({
   children,
@@ -12,7 +13,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <NavBar/>
+        {children}
+      </body>
     </html>
   )
 }
